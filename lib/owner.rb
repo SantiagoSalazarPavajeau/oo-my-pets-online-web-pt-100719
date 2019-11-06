283dbc41b6bc6ef4b9bd3194b69fa26137d8c60a
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-11-06 22:59:37 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -71,7 +71,7 @@ class Owner
   end
   
   def list_pets
-    p "I have #{self.dogs.count} dog(s), and #{self.cats} cat(s)."
+    p "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
   end
     
 end
