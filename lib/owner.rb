01952c8e25a62a886637f087036e67279bacd2e8
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-11-06 22:36:29 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,53 @@
 class Owner
-  # code goes here
+  
+  attr_accessor :cats, :dogs
+  
+  attr_reader :name, :species
+  
+  @@all = []
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+  end
+  
+  def say_species
+    p "I am a #{@species}."
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@all.count
+  end
+  
+  def self.reset_all
+    @@all = []
+  end
+  
+  def cats
+    Cat.all.select {|cats| cats.owner == self}
+  end
+  
+  def dogs
+    Dog.all.select {|dogs| dogs.owner == self}
+  end
+  
+  def buy_cat(name)
+    cat = Cat.new(name, self)
+    self.cats = cat
+  end
+  
+  def buy_dog(name)
+    dog = Dog.new(name, self)
+    self.dogs = dog
+  end
+  
+  def walk_dogs
+    dog = Dog.new(name, self)
+    self.dogs = dog
+    dog.mood = "happy"
+  end
 end
